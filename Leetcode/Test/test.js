4487e333d62e22d5b4de3b7ff65181bdee08dae3
Liny777 <1093742432@qq.com>
2022-01-26 15:01:58 +0800

    1.26

--- Leetcode/Test/test.js
+++ Leetcode/Test/test.js
@@ -100,6 +100,82 @@
 // };
 // backspaceCompare('ab#c')
 
-var a = +Infinity
-var b = 1
-console.log(a>b);
+// var a = +Infinity
+// var b = 1
+// console.log(a>b);
+// /**
+//  * @param {number} target
+//  * @param {number[]} nums
+//  * @return {number}
+//  */
+//  var minSubArrayLen = function(s, nums) {
+
+//     // 二分查找
+//        var binarySearch = function(nums,target){
+//     let left=-1,right = nums.length,middle = 0
+//     while(left+1!=right){
+//         middle = Math.floor((left+right)/2)
+//         if(nums[middle]>=target){
+//             right = middle;
+//         }else{
+//             left = middle
+//         }
+//     }
+//     return right==nums.length?-1:right
+// }
+// // ----------------------------------
+//    let length=nums.length,min=+Infinity,sums=[]
+//    sums[0] = 0
+//    for(let i=1;i<=length;i++){
+//        sums[i] = sums[i-1] + nums[i-1]
+//    }
+//    console.log("s: "+s);
+//    console.log("sums[0]: "+sums[0]);
+//    for(let i=0;i<=length;i++){
+//        let target = s + sums[i]
+//        console.log(i+" sums[i]: "+sums[i]);
+//        console.log("target: "+target);
+//        let index = binarySearch(sums,target)
+//        console.log("index: "+index)
+//        if(index<0){
+//           break
+//        }
+//        if( index<=length){
+//            min = Math.min(min,index-i)
+//            console.log("min: "+min)
+//        }
+//        console.log("----------");
+//    }
+//    return min == +Infinity?0:min
+// };
+// console.log(minSubArrayLen(7,[2,3,1,2,4,3]))
+
+/**
+ * @param {number[]} fruits
+ * @return {number}
+ */
+ var totalFruit = function(fruits) {
+    let blanket = [],result=0;
+    for(let i=0;i<fruits.length;i++){
+        blanket.length = 0
+        for(let j=i;j<fruits.length;j++){
+            if(blanket.length<2){
+                blanket.push(fruits[j])
+            }else{
+                console.log("blanket.indexOf(fruits[j]): "+blanket.indexOf(fruits[j]));
+                if(blanket.indexOf(fruits[j])!==-1){
+                    blanket.push(fruits[j])
+                }else{
+                    console.log("result: "+result);
+                    console.log("blanket.length: "+blanket.length);
+                    result = Math.max(result,blanket.length)
+                    break
+                }
+            }
+           
+
+        }
+    }
+    return result
+};
+console.log(totalFruit([1,2,1]));
